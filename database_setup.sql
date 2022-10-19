92513c12bc4fef69b8ea9374e461c5b67765506c
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-18 22:52:16 -0300

    Initial commit - index and database setup, dependencies installation, files structure defined

---
+++ database_setup.sql
@@ -0,0 +1,93 @@
+CREATE TABLE "public.users" (
+	"id" serial NOT NULL,
+	"name" varchar(50) NOT NULL UNIQUE,
+	"email" varchar(100) NOT NULL UNIQUE,
+	"password" varchar(50) NOT NULL,
+	"picture" TEXT NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"deletedAt" TIMESTAMP DEFAULT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.sessions" (
+	"id" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"deletedAt" TIMESTAMP DEFAULT NULL,
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.hashtags" (
+	"id" serial NOT NULL,
+	"name" varchar(100) NOT NULL UNIQUE,
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"deletedAt" TIMESTAMP DEFAULT NULL,
+	CONSTRAINT "hashtags_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.posts" (
+	"id" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"url" TEXT NOT NULL,
+	"description" varchar(250),
+	"createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"deletedAt" TIMESTAMP DEFAULT NULL,
+	CONSTRAINT "posts_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.likes" (
+	"id" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"postId" integer NOT NULL,
+	CONSTRAINT "likes_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.postsHashtags" (
+	"id" serial NOT NULL,
+	"hashtagId" integer NOT NULL,
+	"postId" integer NOT NULL,
+	CONSTRAINT "postsHashtags_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+
+
+ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+
+ALTER TABLE "likes" ADD CONSTRAINT "likes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("id");
+ALTER TABLE "likes" ADD CONSTRAINT "likes_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("id");
+
+ALTER TABLE "postsHashtags" ADD CONSTRAINT "postsHashtags_fk0" FOREIGN KEY ("hashtagId") REFERENCES "hashtags"("id");
+ALTER TABLE "postsHashtags" ADD CONSTRAINT "postsHashtags_fk1" FOREIGN KEY ("postId") REFERENCES "posts"("id");
+
+
+
+
+
+
+
