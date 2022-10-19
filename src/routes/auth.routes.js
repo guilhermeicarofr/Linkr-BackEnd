5edaa9b77bc6bacd3b29cff4c77ed5beea6d4b71
Francksuel Machado <107144900+Francksuel@users.noreply.github.com>
2022-10-19 19:08:46 -0300

    fix: changing archive name

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 import { signUp } from "../controllers/auth.controllers.js";
 import { validateSignUp } from "../middlewares/validate.auth.js";
 
-const authRoutes = express.Router();
-authRoutes.post("/sign-up", validateSignUp, signUp);
+const authRouter = express.Router();
+authRouter.post("/sign-up", validateSignUp, signUp);
 
-export {authRoutes};
+export {authRouter};
