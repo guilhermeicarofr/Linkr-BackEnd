e3b7380ae58968e46bcbca24d71a1b6fc79594e5
henriquesdias <henrique.dias1915@gmail.com>
2022-10-20 13:37:24 -0300

    fix: it's not possible register a user with name that already in use

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import {
   insertUser,
   getUserByEmail,
+  getUserByName,
 } from "../repositories/auth.repositories.js";
 
 async function signUp(req, res) {
@@ -12,6 +13,8 @@ async function signUp(req, res) {
   try {
     const user = await getUserByEmail(email);
     if (user.rowCount !== 0) return res.sendStatus(409);
+    const nameUser = await getUserByName(name);
+    if (nameUser.rowCount !== 0) return res.sendStatus(409);
     await insertUser(email, name, passwordHash, picture);
     res.sendStatus(201);
   } catch (error) {
