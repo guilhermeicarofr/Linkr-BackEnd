e7db311589a477c679e151f2d0292806665c99d8
Tissyane <tissyane@gmail.com>
2022-10-19 23:15:17 -0300

    feat(post): Created postRouter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ dotenv.config();
 
 import { authRouter } from "./routes/auth.routes.js";
 import { userRouter } from "./routes/user.routes.js";
+import { postRouter } from "./routes/post.routes.js";
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(authRouter);
 server.use(userRouter);
+server.use(postRouter);
 
 server.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
---
+++ src/routes/post.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createPost } from "../controllers/post.controller.js";
+
+const postRouter = express.Router();
+
+postRouter.post("/post", createPost);
+
+export { postRouter };
