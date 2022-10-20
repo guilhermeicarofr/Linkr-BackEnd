2e08604340a3cbfa7e06abeecf7486eeea868a0b
Tissyane <tissyane@gmail.com>
2022-10-19 23:28:00 -0300

    Created postRepository (missing userId from token validation)

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,8 +1,8 @@
-import { insertNewPost } from "../repositories/post.repositories.js";
+import { insertNewPost } from "../repositories/post.repository.js";
 
 const createPost = async (req, res) => {
-  // TODO:Pegar o userId da validação(middleware)
-  //const userId = res.locals
+  // TODO:Pegar o userId da validação(middleware) e corrigir no repository
+  //const userId = res.locals;
   const { url, description } = req.body;
 
   try {
