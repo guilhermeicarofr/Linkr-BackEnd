3f23effc25b3363932b49faee49c4d93f740c44e
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-20 04:39:15 -0300

    feat: also adding metadata scraping to users/id route, similar to timeline

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,3 +1,5 @@
+import urlMetadata from "url-metadata";
+
 import { getUserPostsRepository, getUsersRepository } from "../repositories/user.repositories.js";
 
 const userPostsController = async (req, res) => {
@@ -12,11 +14,24 @@ const userPostsController = async (req, res) => {
 
         if(userPosts.rowCount === 0) return res.sendStatus(404);
 
+        const completePosts = await Promise.all(userPosts.rows.map(async (post) => {
+            const { title, image, url, description } = await urlMetadata(post.url);
+            return ({
+              ...post,
+              url: {
+                title,
+                image,
+                path: url,
+                description
+              }
+            });
+          }));
+
         const objectResponse = {
             id: user.rows[0].id,
             name: user.rows[0].name,
             picture: user.rows[0].picture,
-            posts: userPosts.rows
+            posts: completePosts
         }
 
         return res.status(200).send(objectResponse);
