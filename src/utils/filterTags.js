51180a224ac1b01a9afc9178cdd994c50e0741ea
Tissyane <tissyane@gmail.com>
2022-10-24 16:42:20 -0300

    feat(fix): removed unnecessary validation for users posts

--- src/utils/filterTags.js
+++ src/utils/filterTags.js
@@ -1,8 +1,9 @@
 function filterTags(string) {
-    const taglist = string.split(' ')
-    .filter((word) => word[0]==='#' && word.length > 1)
+  const taglist = string
+    .split(" ")
+    .filter((word) => word[0] === "#" && word.length > 1)
     .map((word) => word.slice(1));
-    return taglist.filter((word, index) => taglist.indexOf(word)===index);
+  return taglist.filter((word, index) => taglist.indexOf(word) === index);
 }
 
 export { filterTags };
