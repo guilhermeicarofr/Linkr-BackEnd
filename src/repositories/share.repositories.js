8b387ece29546544abe3c5dd952cfecc385e751a
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-10-27 20:22:49 -0300

    feat: unshare a post feature added

--- src/repositories/share.repositories.js
+++ src/repositories/share.repositories.js
@@ -8,4 +8,33 @@ async function countPostShares(postId) {
     return db.query(`SELECT COUNT(share.id) AS "shareCount" FROM share WHERE share."postId"=$1;`, [ postId ]);
 }
 
-export { insertNewShare, countPostShares };
+async function getSharebyId(shareId) {
+    return db.query(`
+        SELECT 
+            p."userId" AS "userId",
+            u.name,
+            u.picture,
+            p.id AS "postId",
+            p.description,
+            p.url,
+            s."createdAt",
+            s.id AS "shareId",
+            s."userId" AS "shareUserId",
+            us."name" AS "shareUserName"
+        FROM share s
+        JOIN users us ON s."userId"=us.id
+        JOIN posts p ON s."postId"=p.id
+        JOIN users u ON p."userId"=u.id
+        WHERE p."deletedAt" IS NULL AND s.id=$1;
+        ;`, [ shareId ]);
+}
+
+async function deleteShare(shareId) {
+    return db.query(`
+        DELETE
+        FROM share s
+        WHERE s.id=$1
+        ;`, [shareId]);
+}
+
+export { insertNewShare, countPostShares, getSharebyId, deleteShare };
